559a3ebc81b1d9c7c641587b1f94b21a219419de
Ed <edmccormic@gmail.com>
2017-03-22 21:27:51 -0600

    first commit

---
+++ Sources/App/main.swift
@@ -0,0 +1,40 @@
+import Vapor
+
+let drop = Droplet()
+
+drop.get { request in
+    // return "Hello, Vapor!"
+    return try JSON(node:["message": "Hello, Vapor!"
+        ])
+}
+
+drop.get("hello") { request in
+    // return "Hello, Vapor!"
+    return try JSON(node:["message": "Hello Again!"
+        ])
+}
+
+drop.get("hello", "there") { request in
+    // return "Hello, Vapor!"
+    return try JSON(node:["message": "I am tired!"
+        ])
+}
+
+//drop.get("beers", Int.self) { request, beers in
+//    return try JSON(node: [
+//        "message": "Take one down, pass it around, \(beers - 1) bottles of beer on the wall..."
+//        ])
+//}
+
+drop.post("post") { request in
+    guard let name = request.data["name"]?.string else {
+        throw Abort.badRequest
+    }
+    return try JSON(node: [
+        "message": "Hello, \(name)!"
+    ])
+}
+
+drop.resource("posts", PostController())
+
+drop.run()
